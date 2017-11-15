184b013cd6f640f50a38ca52a902c9e7e819d613
qingxue <173636717@qq.com>
2017-11-16 00:35:00 +0800

    Initial Commit

---
+++ Notification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notification
+//
+//  Created by qingxue on 17/11/16.
+//  Copyright © 2017年 qingxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Notification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Notification
+//
+//  Created by qingxue on 17/11/16.
+//  Copyright © 2017年 qingxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
